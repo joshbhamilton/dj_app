4d27f079ee75ec10fc56bc3e73ca1a5c74ad5b43
Josh Hamilton <joshb.hamilton@gmail.com>
2011-07-05 14:05:06 -0700

    moved to 120

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   def combine_for_publishing
     logger.info "Combining for publishing #{self.title}..."
 #    sleep_time = rand(60)
-    sleep_time = 75
+    sleep_time = 120
     sleep(sleep_time)
     self.published = true
     self.save!
